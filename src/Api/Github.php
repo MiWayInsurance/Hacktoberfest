4db138bb249e0a6f52fe82d606e7a1e5a8ca47e5
Pierre du Plessis <pierre@pcservice.co.za>
2017-10-06 21:47:44 +0200

    Add extra level of cache to github queries

--- src/Api/Github.php
+++ src/Api/Github.php
@@ -10,15 +10,15 @@ use Symfony\Component\Yaml\Yaml;
 class Github
 {
     private $client;
-
     private $projectDir;
+    private $cache;
 
     public function __construct($projectDir, $cacheDir)
     {
         $this->client = new Client();
         $this->projectDir = $projectDir;
-        $cache = new FilesystemAdapter('', 60 * 60, $cacheDir);
-        $this->client->addCache($cache);
+        $this->cache = new FilesystemAdapter('', 3600, $cacheDir);
+        $this->client->addCache($this->cache);
     }
 
     public function getDeveloperList(): array
@@ -30,19 +30,31 @@ class Github
         foreach ($this->getTeamsList() as $team => $developers) {
             $prs[$team] = [];
             foreach ($developers as $author) {
-                $params = [
-                    '-label' => 'invalid',
-                    'created' => '2017-09-30T00:00:00-12:00..2017-10-31T23:59:59-12:00',
-                    'type' => 'pr',
-                    'is' => 'public',
-                    'author' => $author,
-                ];
 
-                $issues = $this->client->api('search')->issues(implode(' ', array_map(function ($k, $v) {
-                    return "$k:$v";
-                }, array_keys($params), array_values($params))));
+                $cache = $this->cache->getItem($author);
 
-                $user = $this->client->users()->show($author);
+                if ($cache->isHit()) {
+                    $issues = $cache->get()['issues'];
+                    $user = $cache->get()['user'];
+                } else {
+                    $params = [
+                        '-label' => 'invalid',
+                        'created' => '2017-09-30T00:00:00-12:00..2017-10-31T23:59:59-12:00',
+                        'type' => 'pr',
+                        'is' => 'public',
+                        'author' => $author,
+                    ];
+
+                    $issues = $this->client->api('search')->issues(implode(' ', array_map(function ($k, $v) {
+                        return "$k:$v";
+                    }, array_keys($params), array_values($params))));
+
+                    $user = $this->client->users()->show($author);
+
+                    $cache->set(['issues' => $issues, 'user' => $user]);
+                    $this->cache->saveDeferred($cache);
+                }
+
                 $prs[$team][$author] = [
                     'total' => $issues['total_count'],
                     'avatar' => $user['avatar_url'],
@@ -50,6 +62,8 @@ class Github
                 ];
             }
         }
+
+        $this->cache->commit();
 
         return $prs;
     }
